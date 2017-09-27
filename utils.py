52834b089c6a46a113baf35cffd7950a50205440
joseluisvaz <joseluisv1201@gmail.com>
2017-09-26 20:56:15 -0500

    adding matplotlib as import

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy
 import datetime as dt
 import pandas as pd
 import calendar
+import matplotlib.pyplot as plt
 
 months_dic = { "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3, "May": 4, "Jun": 5, "Jul": 6, "Aug": 7,
             "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11 }
