53faf1621eeb968e86c44a7889ea4e05a2637ce5
joseluisvaz <joseluisv1201@gmail.com>
2017-09-26 23:11:26 -0500

    to start notebook

--- main.py
+++ main.py
@@ -16,49 +16,8 @@ sns.set_style("white")
 data = pd.read_csv('claims.csv')
 data = utils.clean_data(data)
 
-data.dtypes
-
-data[data["AddressChange_Claim"] == "under 6 months"].shape[0]
-utils.plot_variable_percentage(data, "BasePolicy", kind = "bar")
-
-
-Month                            int64
-WeekOfMonth                      int64
-DayOfWeek                        int64
-Make                            object
-AccidentArea                    object
-DayOfWeekClaimed               float64
-MonthClaimed                   float64
-WeekOfMonthClaimed               int64
-Sex                             object
-MaritalStatus                   object
-Age                            float64
-Fault                           object
-PolicyType                      object
-VehicleCategory                 object
-VehiclePrice                    object
-FraudFound_P                     int64
-PolicyNumber                     int64
-RepNumber                        int64
-Deductible                       int64
-DriverRating                     int64
-Days_Policy_Accident            object
-Days_Policy_Claim               object
-PastNumberOfClaims              object
-AgeOfVehicle                    object
-AgeOfPolicyHolder               object
-PoliceReportFiled               object
-WitnessPresent                  object
-AgentType                       object
-NumberOfSuppliments             object
-AddressChange_Claim             object
-NumberOfCars                    object
-Year                             int64
-BasePolicy                      object
-Date                    datetime64[ns]
-DateClaimed             datetime64[ns]
-Delay                            int64
-dtype: object
+data[data["AddressChange_Claim"] == "4 to 8 years"].shape[0]
+utils.plot_variable_percentage(data, "AgeOfVehicle", kind = "bar")
 
 
 utils.plot_variable_percentage_datetime(data, "DateClaimed", 'year', kind = 'bar', style = 'o')
